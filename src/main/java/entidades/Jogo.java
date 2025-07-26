584c898118078404b217f0f10310341d42575677
Alexandre <josealexandre2005@outlook.com>
2025-07-26 10:40:14 -0300

    extends para o produto

--- src/main/java/entidades/Jogo.java
+++ src/main/java/entidades/Jogo.java
@@ -1,77 +1,17 @@
 package entidades;
-public class Jogo {
-    private String codigo;
-    private String descricao;
-    private String marca;
-    private int valorEntrada;
-    private int valorSaida;
-    private int qtdEstoque;
-    private String nome;
+public class Jogo extends Produto {
+    private String genero;
 
-    public Jogo(String codigo, String descricao, String marca, int valorEntrada, int valorSaida, int
-            qtdEstoque, String nome) {
-        this.codigo = codigo;
-        this.descricao = descricao;
-        this.marca = marca;
-        this.valorEntrada = valorEntrada;
-        this.valorSaida = valorSaida;
-        this.qtdEstoque = qtdEstoque;
-        this.nome = nome;
+    public Jogo(String codigo, String descricao, String marca, int valorEntrada, int valorSaida, int qtdEstoque, String nome, String genero) {
+        super(codigo, descricao, marca, valorEntrada, valorSaida, qtdEstoque, nome);
+        this.genero = genero;
     }
 
-    public String getCodigo() {
-        return codigo;
+    public String getGenero() {
+        return genero;
     }
 
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public int getValorEntrada() {
-        return valorEntrada;
-    }
-
-    public void setValorEntrada(int valorEntrada) {
-        this.valorEntrada = valorEntrada;
-    }
-
-    public int getValorSaida() {
-        return valorSaida;
-    }
-
-    public void setValorSaida(int valorSaida) {
-        this.valorSaida = valorSaida;
-    }
-
-    public int getQtdEstoque() {
-        return qtdEstoque;
-    }
-
-    public void setQtdEstoque(int qtdEstoque) {
-        this.qtdEstoque = qtdEstoque;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setGenero(String genero) {
+        this.genero = genero;
     }
 }

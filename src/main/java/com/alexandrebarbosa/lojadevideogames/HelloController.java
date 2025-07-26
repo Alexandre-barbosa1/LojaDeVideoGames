9212f24177a2ae76a373c22c4d0315d7d61eca6a
Marcos Vinicius dos Santos Xavier <marcosvinixavier123@gmail.com>
2025-07-25 22:42:23 -0300

    Adicionei Segunda Pagina

--- src/main/java/com/alexandrebarbosa/lojadevideogames/HelloController.java
+++ src/main/java/com/alexandrebarbosa/lojadevideogames/HelloController.java
@@ -1,7 +1,13 @@
 package com.alexandrebarbosa.lojadevideogames;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -9,6 +15,31 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("só um teste");
+        welcomeText.setText("Bem-vindo à Loja de Videogames!");
     }
+
+
+    @FXML
+    protected void onActionGamesButtonClick() {
+        welcomeText.setText("Você clicou em Jogos de Ação!");
+
+    }
+
+
+    @FXML
+    protected void onStrategyGamesButtonClick() {
+        welcomeText.setText("Você clicou em Jogos de Estratégia!");
+
+    }
+    @FXML
+    protected void onOpenSecondWindowButtonClick(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SecondView.fxml"));
+        Scene secondScene = new Scene(fxmlLoader.load());
+        Stage currentStage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        currentStage.setScene(secondScene);
+        currentStage.setTitle("Minha Segunda Janela");
+        currentStage.show();
+    }
+
 }

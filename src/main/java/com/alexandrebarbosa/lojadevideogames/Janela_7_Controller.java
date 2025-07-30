780c343c92e957f0669a8e027c96436af179bd8f
Antônio F. O. Neto <antonio.fer.oli.neto@gmail.com>
2025-07-30 20:25:28 -0300

    .

---
+++ src/main/java/com/alexandrebarbosa/lojadevideogames/Janela_7_Controller.java
@@ -0,0 +1,45 @@
+package com.alexandrebarbosa.lojadevideogames;
+
+import entidades.Jogo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.io.IOException;
+
+import static com.alexandrebarbosa.lojadevideogames.Janela_2_Controller.jogos;
+
+public class Janela_7_Controller {
+
+    @FXML
+    private TableView<Jogo> tabelaItens;
+    @FXML
+    private TableColumn<Jogo, String> colunaNome;
+    @FXML
+    private TableColumn<Jogo, Integer> colunaQuantidadeEstoque;
+    @FXML
+    private Label totalEstoque;
+    @FXML
+    public void initialize() {
+        colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        colunaQuantidadeEstoque.setCellValueFactory(new PropertyValueFactory<>("quantidadeEstoque"));
+        ObservableList<Jogo> dados = FXCollections.observableArrayList(jogos);
+        tabelaItens.setItems(dados);
+        int total = 0;
+        for (int i=0; i<jogos.size(); i++) {
+            total += jogos.get(i).getQuantidadeEstoque();
+        }
+        totalEstoque.setText("Total de estoque: " + total);
+    }
+
+    @FXML
+    protected void Voltar(ActionEvent event) throws IOException {
+        Navegacao.remover();
+        Navegacao.caminho(event);
+    }
+}
